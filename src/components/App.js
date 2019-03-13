e0eae6442d7c7f976204e02e4a9a3ac13dd7dd54
Justin Fry <mrjustinfry@gmail.com>
2019-03-13 14:34:38 -0400

    inital landing page w/assets

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-
 import '../style/App.css';
+import Landing from './Landing';
 
 class App extends Component {
   render() {
@@ -11,8 +11,8 @@ class App extends Component {
                <main>
                  <Switch>
                    <Route
-                      exact path=""
-                      //component={}
+                      exact path="/"
+                      component={Landing}
                    />
                    <Route
                       exact path=""
